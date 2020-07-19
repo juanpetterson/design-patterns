cc239ed3b6dc2b671ee82c4b3f2ee67d483e5fe3
juanpetterson <juan.petterson@gmail.com>
2020-07-18 21:01:35 -0300

    add builder pattern with intro and example

---
+++ PATTERNS/BUILDER/index.js
@@ -0,0 +1,89 @@
+class Person {
+  constructor() {
+    // address
+    this.streetAdrdress = this.postcode = this.citu = '';
+
+    // employment
+    this.companyName = this.position = '';
+    this.annualIncome = 0;
+  }
+
+  toString() {
+    return (
+      `Person lives at ${this.streetAddress}, ${this.city}, ${this.postcode}\n` +
+      `and works at ${this.companyName} as a ${this.position} earning ${this.annualIncome}`
+    );
+  }
+}
+
+class PersonBuilder {
+  constructor(person = new Person()) {
+    this.person = person;
+  }
+
+  get lives() {
+    return new PersonAddressBuilder(this.person);
+  }
+
+  get works() {
+    return new PersonJobBuilder(this.person);
+  }
+
+  build() {
+    return this.person;
+  }
+}
+
+class PersonJobBuilder extends PersonBuilder {
+  constructor(person) {
+    super(person);
+  }
+
+  at(companyName) {
+    this.person.companyName = companyName;
+    return this;
+  }
+
+  asA(position) {
+    this.person.position = position;
+    return this;
+  }
+
+  earning(annualIncome) {
+    this.person.annualIncome = annualIncome;
+    return this;
+  }
+}
+
+class PersonAddressBuilder extends PersonBuilder {
+  constructor(person) {
+    super(person);
+  }
+
+  at(streetAddress) {
+    this.person.streetAddress = streetAddress;
+    return this;
+  }
+
+  withPostcode(postcode) {
+    this.person.postcode = postcode;
+    return this;
+  }
+
+  in(city) {
+    this.person.city = city;
+    return this;
+  }
+}
+
+let personBuilder = new PersonBuilder();
+let person = personBuilder.lives
+  .at('123 London Road')
+  .in('London')
+  .withPostcode('312312')
+  .works.at('Fabrikam')
+  .asA('Engineer')
+  .earning(123123)
+  .build();
+
+console.log(person.toString());
